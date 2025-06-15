4af98dda9e79ea5327d337c42d072fb3e6f75c0d
iuliivasilev <yuliivasiliev@gmail.com>
2025-06-15 13:13:13 +0300

    Prepared first version of README

---
+++ setup.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def create_readme():
+    df = pd.read_excel('./source/JournalsQ1Q2.xlsx')
+    df = df.fillna('')
+    markdown_table = df.to_markdown(index=False)
+
+    with open('./source/Canvas.md', 'r') as file:
+        content = file.readlines()
+
+    start_index = content.index("## Q1-Q2 Journals\n") + 1
+    content.insert(start_index, "\n### Main Journal Table\n" + markdown_table + "\n\n")
+
+    with open('./README.md', 'w') as file:
+        file.writelines(content)
+
+if __name__ == "__main__":
+    create_readme()
